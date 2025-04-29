db31d551ed7fc1f3195e9fc04e4da3e15a30dca0
Danilo Rodrigues Lopes <Danilorodrigues987.drl@gmail.com>
2025-04-29 11:59:19 -0300

    Add files via upload

---
+++ Enter-A-Two-Numbers-Format.py
@@ -0,0 +1,5 @@
+n1 = int(input('Digite um número:'))
+n2 = int(input('Digite outro número:'))
+v3 = n1 + n2
+#print('A soma do número ','"',n1,'"',' + o número ','"',n2,'"',' resulta em: "{}"' .format(v3))
+print('A soma do número "{}" + o número "{}" resulta no número "{}"' .format(n1, n2, v3))
